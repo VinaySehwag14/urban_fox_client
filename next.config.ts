fcf377a78885409d38fe3559d225940007321dd6
Vinay_Sehwag <vinay_sehwag@Developers-MacBook-Air.local>
2025-11-27 19:04:12 +0530

    fixed redirection issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/jugaducollection",
+        destination: "https://www.jugaaducollection.com",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
